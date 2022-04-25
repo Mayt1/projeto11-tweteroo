ca8c9e31ebe9395e6ed4af2630e0ba89fdaa6c02
VictorUlian <yctor.ulian@gmail.com>
2022-04-25 13:30:25 -0300

    Feature: Can request at endpoint /tweets via GET

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     const {username, tweet} = req.body;
     if(username && tweet) {
-        //let userAvatar = findUserImage(username)
         let userAvatar = user.find(user => user.username === username)
         tweets.push({ username: username, tweet: tweet, avatar: userAvatar.avatar});
         res.send("OK");  
@@ -33,9 +32,89 @@ app.post('/tweets', (req, res) => {
     console.log(tweets);
 });
 
-
+app.get('/tweets', (req, res) => {
+    res.send(tweets.slice(tweets.length-10, tweets.length));
+});
 
 
 app.listen(5000, ()=> {
     console.log("Back-end funcionando, nao esquece de desligar a cada atualizaçao")
 });
+
+
+/* TEST OBJECTS
+let user = [
+    {
+        "username": "carinha",
+        "avatar": "aquele avatar"
+    },
+    {
+        "username": "flasco",
+        "avatar": "uniao flasco"
+    }
+];
+
+let tweets = [
+    {
+        "username": "flasco",
+        "tweet": "0",
+        "avatar": "uniao flasco"
+    },
+    {
+        "username": "carinha",
+        "tweet": "1"
+        "avatar": "aquele avatar"
+    },
+    {
+        "username": "flasco",
+        "tweet": "2",
+        "avatar": "uniao flasco"
+    },
+    {
+        "username": "carinha",
+        "tweet": "3"
+        "avatar": "aquele avatar"
+    },
+    {
+        "username": "flasco",
+        "tweet": "4",
+        "avatar": "uniao flasco"
+    },
+    {
+        "username": "carinha",
+        "tweet": "5"
+        "avatar": "aquele avatar"
+    },
+    {
+        "username": "flasco",
+        "tweet": "6",
+        "avatar": "uniao flasco"
+    },
+    {
+        "username": "carinha",
+        "tweet": "7"
+        "avatar": "aquele avatar"
+    },
+    {
+        "username": "flasco",
+        "tweet": "8",
+        "avatar": "uniao flasco"
+    },
+    {
+        "username": "carinha",
+        "tweet": "9"
+        "avatar": "aquele avatar"
+    },
+    {
+        "username": "flasco",
+        "tweet": "10",
+        "avatar": "uniao flasco"
+    },
+    {
+        "username": "flasco",
+        "tweet": "11",
+        "avatar": "uniao flasco"
+    }
+]
+
+*/

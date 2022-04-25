0edd62e57279aa3f84203493e7f90d1152563a4e
VictorUlian <yctor.ulian@gmail.com>
2022-04-25 13:07:35 -0300

    Fix: Server can add the user avatar at endpoint /tweets in POST

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 
 let user = [];
 let tweets = [];
@@ -22,14 +23,15 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     const {username, tweet} = req.body;
     if(username && tweet) {
-        tweets.push({ username: username, tweet: tweet});
+        //let userAvatar = findUserImage(username)
+        let userAvatar = user.find(user => user.username === username)
+        tweets.push({ username: username, tweet: tweet, avatar: userAvatar.avatar});
         res.send("OK");  
     } else {
         res.send("todos os campos sao obrigatorios!")
     }
     console.log(tweets);
 });
-
 
 
 

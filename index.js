e583987025bd116d207fbec37535bcea38156ff3
VictorUlian <yctor.ulian@gmail.com>
2022-04-25 12:19:12 -0300

    feature: Can request at endpoint /sign-up via POST

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-//Back-end Holy
-
 import express from 'express';
 import cors from 'cors';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
+
+let user = [];
+
+app.post('/sign-up', (req, res) => {
+    const {username, avatar} = req.body;
+    if(username && avatar) {
+        user.push({ username: username, avatar: avatar});
+        res.send("OK");  
+    } else {
+        res.send("todos os campos sao obrigatorios!")
+    }
+    console.log(user);
+});
 
 
 app.listen(5000, ()=> {

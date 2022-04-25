4f39f1888fef97582ceec112bf4cb0acbcc37534
VictorUlian <yctor.ulian@gmail.com>
2022-04-25 12:33:10 -0300

    Feature: Can request at endpoint /tweets via POST

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-
 app.use(express.json());
-
 app.use(cors());
 
 let user = [];
+let tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const {username, avatar} = req.body;
@@ -19,6 +18,20 @@ app.post('/sign-up', (req, res) => {
     }
     console.log(user);
 });
+
+app.post('/tweets', (req, res) => {
+    const {username, tweet} = req.body;
+    if(username && tweet) {
+        tweets.push({ username: username, tweet: tweet});
+        res.send("OK");  
+    } else {
+        res.send("todos os campos sao obrigatorios!")
+    }
+    console.log(tweets);
+});
+
+
+
 
 
 app.listen(5000, ()=> {
